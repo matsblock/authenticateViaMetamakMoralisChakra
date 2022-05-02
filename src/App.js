b884d031eb2ce26de223c51eef750cd53557ab1f
matsblock <matsblock@outlook.com>
2022-05-02 10:51:57 -0300

    Working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import { Button, Alert, AlertIcon, AlertTitle, AlertDescription, Box, CloseButton} from '@chakra-ui/react';
 import { Container, Heading } from '@chakra-ui/react';
-import { useMoralis } from 'react-moralis';
+//import Moralis from 'moralis/types';
+import { useMoralis, useWeb3ExecuteFunction } from 'react-moralis';
 
 
 function App() {
-  const { authenticate, isAuthenticated, authError, logout, isAuthenticating, user } = useMoralis();
+  const { Moralis, authenticate, isAuthenticated, authError, logout, isAuthenticating, user } = useMoralis();
+
+  const enableWeb3 = useWeb3ExecuteFunction();
+
+  const multiplicar = async () => {
+    console.log("multiplicar")
+
+    let options = {
+      contractAddress: "0x5a54419Dce08F2304f71c90B51A41F28cCb60B98",
+      functionName: "multiplicar",
+      abi:	[{
+        "inputs": [],
+        "name": "multiplicar",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }],
+      params: {},
+      msgValue: 0
+    }
+    await Moralis.executeFunction(options);
+  }
+
 
   if (isAuthenticated) {
     return (
       <Container>
-        <Heading>Welcome, {user.attributes.username}</Heading>
+        <Heading>Welcome, {user.attributes.username}, This is a Rinkeby test</Heading>
+        <Button onClick={() => multiplicar()}>multiplicar</Button>
         <Button onClick={() => logout()}>Logout</Button>
       </Container>
     )
@@ -36,6 +60,7 @@ function App() {
       </Alert>
       )}
       <Button isLoading={isAuthenticating} onClick={() => authenticate()}>Authenticate via Metamask</Button>
+
 
     </Container>
   );
